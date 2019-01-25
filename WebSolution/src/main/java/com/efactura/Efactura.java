3c4d932804a43f6a3954f2553da70c789659f2c9
jcamacho <jcamacho@vectoritcgroup.com>
2019-01-25 18:15:18 +0100

    deploy tomcat

--- WebSolution/src/main/java/com/efactura/Efactura.java
+++ WebSolution/src/main/java/com/efactura/Efactura.java
@@ -3,12 +3,15 @@ package com.efactura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.efactura"}) 
-public class Efactura {
+@EntityScan("com.efactura.*") 
+public class Efactura extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Efactura.class, args);
